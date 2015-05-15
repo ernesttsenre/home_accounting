61dc14665c3942804944daaabec34dcfacc72d0c
Иванов Олег <ernest.oleg.iv@gmail.com>
2015-05-15 14:20:40 +0700

    фикс

--- django_project/settings_prod.py
+++ django_project/settings_prod.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = '/home/django/django_project/static'
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + '/static'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )

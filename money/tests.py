52628b85228d24cb7eb736387065fbd3a66e1492
Иванов Олег <ernest.oleg.iv@gmail.com>
2015-05-22 15:40:56 +0700

    добавил первый тест

--- money/tests.py
+++ money/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from money.models import Transfer
 
-# Create your tests here.
+
+class BaseTest(TestCase):
+    def test_testing_system(self):
+        return self.assertEqual(True, True)
+
+
+# class TransferTest(TestCase):
+#     def test_create_operations_for_accounts_when_transfer_created(self):
+#         """
+#         Когда создается перевод денег, он обязан создать операцию расхода на кредитном счету
+#         и операцию прихода, на туже сумму, на дебиторном счету
+#         :return:
+#         """
+#
+#         account =
+#         transfer = Transfer()

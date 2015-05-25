48d52269abe27a9e1faa30abc58cdd8272e8ec0e
Иванов Олег <ernest.oleg.iv@gmail.com>
2015-05-25 21:39:57 +0700

    фикс

--- templates/static/js/app.js
+++ templates/static/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-   $('body').on('click', 'button[type="submit"]', function(e) {
-       $(this).button('loading');
-   })
+   //$('body').on('click', 'button[type="submit"]', function(e) {
+   //    $(this).button('loading');
+   //})
 });

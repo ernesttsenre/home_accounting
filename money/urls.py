313a53d96bca5a8f6f1dd244714e4295cf9ccf50
Иванов Олег <ernest.oleg.iv@gmail.com>
2015-06-02 15:12:25 +0700

    Небольшой фикс

--- money/urls.py
+++ money/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^operation/create/(?P<account_id>[0-9]+)$', login_required(OperationCreate.as_view()),
         name='create_operation'),
     url(r'^transfer/create/$', login_required(TransferCreate.as_view()), name='create_transfer'),
-    url(r'^operation/archive/(?P<year>[0-9]{4})/(?P<month>[0-9]+)/$', OperationMonthArchiveView.as_view(month_format='%m'),
-        name="operation_month_archive"),
+    url(r'^operation/archive/(?P<year>[0-9]{4})/(?P<month>[0-9]+)/$',
+        OperationMonthArchiveView.as_view(month_format='%m'), name="operation_month_archive"),
 ]
